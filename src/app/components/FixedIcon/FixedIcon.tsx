b2b492e9f0c3606f6448d84895f7b95cbe303e7e
YoussefKhaildAli <yossofwd@gmail.com>
2025-06-10 21:12:42 +0300

    Fixed number

--- src/app/components/FixedIcon/FixedIcon.tsx
+++ src/app/components/FixedIcon/FixedIcon.tsx
@@ -3,7 +3,7 @@ import "./FixedIcon.css";
 export default function FixedIcon() {
   return (
     <a
-      href="https://wa.me/971501234567"
+      href="https://wa.me/+97142619871"
       target="_blank"
       aria-label="Chat with us on WhatsApp"
       className="fixed-icon"
