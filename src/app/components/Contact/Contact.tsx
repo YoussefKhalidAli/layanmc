8f280a25181be822f1c14c2d4fdbacf692707b4a
YoussefKhaildAli <yossofwd@gmail.com>
2025-06-17 20:19:43 +0300

    Removed unneccessary line

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -3,10 +3,6 @@ export default function Contact() {
     <section id="contact" className="contact section light-background">
       <div className="container section-title" data-aos="fade-up">
         <h2>Contact</h2>
-        <p>
-          Necessitatibus eius consequatur ex aliquid fuga eum quidem sint
-          consectetur velit
-        </p>
       </div>
       <div className="mb-5" data-aos="fade-up" data-aos-delay="200">
         <iframe
